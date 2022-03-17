c96ea6881ddb3e524143ee5e6f9444ca47d9856c
Ali-Maz201 <mazik.ali94@gmail.com>
2022-03-17 14:26:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const binancePairs = ['BTCUSDT', "ETHUSDT", "BNBUSDT"];
+let interval = "12h";
+let pair;
+
+async function changePair() {
+  pair = document.getElementById("currentPair").value;
+  if(binancePairs.includes(pair.toUpperCase())) {
+    await createChart();
+  }
+}
+async function changeInterval (id) {
+    interval = id;
+    await createChart();
+}
+
+async function createChart(){
+  removeChart();
+  const url = "https://api.binance.com/api/v3/klines?symbol=" + pair.toUpperCase() + "&interval=" + interval + "&limit=1000";
+  const chart = LightweightCharts.createChart(document.getElementById("chart"), { width:1100, height:400 });
+  const candles =  chart.addCandlestickSeries();
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const candlesData = data.map(d => {
+      return {time:d[0]/1000, open:parseFloat(d[1]), high:parseFloat(d[2]), low:parseFloat(d[3]), close:parseFloat(d[4])};
+    });
+    candles.setData(candlesData);
+    document.getElementById('interval').style.display = "block";
+  } catch (Ex) {
+    console.log(Ex);
+  }
+}
+
+function removeChart() {
+  document.getElementById('chart').remove();
+  const chartContainer = document.getElementById('chartContainer');
+  const chart = document.createElement('div');
+  chart.id = "chart";
+  chartContainer.prepend(chart);
+}
